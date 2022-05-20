7de8dead704e9f986a74a048f493f46da809195b
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2022-05-20 21:29:43 +0100

    finished mobile view

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const menu = document.getElementsByClassName("navlinks");
-const dimBackground = document.querySelector('.overlay')
+const dimBackground = document.querySelector('.overlay');
+const subMenu = document.getElementsByClassName('submenu');
+const dropToggle = document.getElementsByClassName('arrow-down')
+const closeToggle = document.getElementsByClassName('arrow-up')
 
 function openMenu(){
 	menu[0].classList.add('active');
@@ -8,6 +11,21 @@ function openMenu(){
 function closeMenu(){
 	menu[0].classList.remove("active");
 	dimBackground.style.display = 'none'
+}
+dimBackground.onclick = ()=> {
+	closeMenu();
+}
+for (let i = 0; i < 2; i++){
+	dropToggle[i].onclick = ()=> {
+		closeToggle[i].style.display = 'block';
+		dropToggle[i].style.display = 'none';
+		subMenu[i].classList.add('active')
+	}
+	closeToggle[i].onclick = ()=> {
+		closeToggle[i].style.display = 'none';
+		dropToggle[i].style.display = 'block';
+		subMenu[i].classList.remove('active');
+	}
 }
 
 
